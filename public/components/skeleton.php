eebfbcf32c70c170c2ca1b78985f07db58b077b9
Kami <kami.labs@proton.me>
2024-05-22 13:40:03 +0200

    SVG Plan updates + IDs + Working interaction with Objects on plan

---
+++ public/components/skeleton.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/global.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <!-- Content will be injected here using laravel framework-->
+        <?= $content ?>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
